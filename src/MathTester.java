2045b60e04c4a0f3ce9ee028325872ee3b3b3622
mohamed hussein <mohamed.hussein.2022@student.lhschools.org>
2019-09-12 09:41:12 -0400

    adding "hello source control" to main method

--- src/MathTester.java
+++ src/MathTester.java
@@ -16,6 +16,7 @@ public class MathTester {
         System.out.println("firstNumber * SecondNumber is "+ (firstNumber*secondNumber));
         System.out.println("secondNumber / firstNumber is "+ (secondNumber/firstNumber));
         System.out.println("the modulus of the secondNumber by the firstNumber"+(firstNumber+secondNumber));
+        System.out.print("Hello World");
 
 
 
