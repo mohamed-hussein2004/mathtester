e72363c11257eb4b0d9e31513fb48168713ce3a2
mohamed hussein <mohamed.hussein.2022@student.lhschools.org>
2019-09-12 09:33:48 -0400

    initial commit

---
+++ src/MathTester.java
@@ -0,0 +1,28 @@
+public class MathTester {
+    public static void main(String[] args){
+        String firstWord;
+        String secondWord;
+        int firstNumber,secondNumber;
+
+        firstWord = "dog";
+        secondWord = "house";
+        firstNumber = 5;
+        secondNumber = 10;
+
+        System.out.println(firstWord+secondWord);
+        System.out.println(firstWord+(firstNumber+secondNumber));
+        System.out.println("firstNumber + SecondNumber is " + (firstNumber+secondNumber));
+        System.out.println("firstNumber - secondNumber is "+ (firstNumber-secondNumber));
+        System.out.println("firstNumber * SecondNumber is "+ (firstNumber*secondNumber));
+        System.out.println("secondNumber / firstNumber is "+ (secondNumber/firstNumber));
+        System.out.println("the modulus of the secondNumber by the firstNumber"+(firstNumber+secondNumber));
+
+
+
+
+    }
+}
+
+
+
+
